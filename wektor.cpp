9b08811e5a0cececb2ffcfe83318d7165cf4bc07
Kalina Kielbratowska <kalina.air@wp.pl>
2018-10-13 15:21:40 +0200

    z laborek

---
+++ wektor.cpp
@@ -0,0 +1,17 @@
+#include <stdio>
+#include <math>
+#include <vector>
+
+std::vector<int> funkcja(std::vector<int> T1,int hi,int lo){
+for(auto element: T1)
+if(element>=lo && element<=hi)
+auto T3.push_back(element);
+}
+
+main()
+{
+auto T3=funkcja({1,2,3,4,5,5,5,6,7,8},6,4};
+cout<<"Wektor:<<T3;
+return 0;
+}
+
